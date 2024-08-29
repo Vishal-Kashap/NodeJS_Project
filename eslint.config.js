b66f856b46f95a834181fa6af7d16acffa6b6e88
Ubuntu <ubuntu@ip-172-31-95-145.ec2.internal>
2024-08-29 12:40:43 +0000

    Specify files to lint in ESLint config file

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  languageOptions: {
+    globals: {
+      // Define global variables if necessary
+    },
+    parserOptions: {
+      ecmaVersion: 2020,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  rules: {
+    // Add any specific rules here if needed
+  },
+};
+
